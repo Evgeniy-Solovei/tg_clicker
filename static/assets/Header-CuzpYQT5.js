e0e87eee9225a07c594ea5a2acb304908c52d3cf
maksimjoyharper <makksss1123@icloud.com>
2024-08-22 22:35:46 +0300

    Check error avtoBot first

--- static/assets/Header-CuzpYQT5.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-B7WMYub0.js";const e="_banner_6gc9a_1",r={banner:e};function s(){return n.jsx("div",{className:r.banner,children:"БАННЕР"})}export{s as H};
---
+++ static/assets/box2-B7gpmBd3.js
@@ -0,0 +1,1 @@
+const s="/assets/box2-_a3OpZPI.png";export{s as i};
